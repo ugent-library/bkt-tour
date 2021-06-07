fdba37a4c21c7a5245a39487558e9ab8030918ab
Matthias Vandermaesen <matthias.vandermaesen@ugent.be>
2021-06-07 16:04:05 +0200

    Initial commit.

---
+++ translations/nl/site.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'Rooms' => 'Ruimtes',
+    'Related articles' => 'Gerelateerde artikelen',
+    'Architects' => 'Architecten',
+    'Librarians' => 'Bibliothecarissen',
+    'Art' => 'Kunst',
+    'Events' => 'Evenementen',
+    'Historical reference' => 'Historische verwijzing',
+    'Start tour' => 'Start rondleiding',
+    'Languages' => 'Talen',
+    'Index of contents' => 'Inhoudstafel',
+    'Map' => 'Kaart'
+];
